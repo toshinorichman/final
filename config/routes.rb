68899b57cbe2793584ad5fe7ac6ac2617a5483ce
Raghu Betina <raghu@firstdraft.com>
2018-12-12 02:08:53 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "upload_pictures#index"
   devise_for :comments
   # Routes for the Comment resource:
